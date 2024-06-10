a01a2527a7340e1256e9053bbd8b7e904fa2d1a0
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-06-10 10:57:31 +0700

    connect to database

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
