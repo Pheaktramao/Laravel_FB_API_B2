ac94bd676a0c675594ac18173f884fe780b90fdd
PHEAKTRA.MAO <PHEAKTRA.MAO@PNC.LAN>
2024-06-14 11:12:12 +0700

    change code for like on the post #5

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,23 +26,4 @@ class Post extends Model
     public function getAllLike(){
         return $this->hasMany(Like::class, 'post_id');
     }
-
-
-    // public static function store($request, $id = null)
-    // {
-    //     $data = $request->only('description', 'image', 'auth_id');
-    //     $data = self::updateOrCreate(['id' => $id], $data);
-    //     return $data;
-    // }
-
-    // public static function show($id){
-    //     $data = self::find($id);
-    //     return $data;
-    // }
-    
-    // public static function destroy($id){
-    //     $data = self::find($id);
-    //     $data->delete();
-    //     return $data;
-    // }
 }
